4d2a2311bfaaf344f5838e1d6d2fad7e8bc7e06f
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2024-02-24 22:21:33 -0300

    Fix dependencies

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,34 +1,6 @@
 import { UnderConstruction } from "@components";
-import {
-  fetchDrivers,
-  fetchDriverById,
-  fetchTrafficViolations,
-  fetchTrafficViolationById,
-  fetchAppeals,
-  fetchAppealById,
-  fetchEmployees,
-  fetchEmployeeById,
-} from "@services";
-import React from "react";
 
 const Dashboard = () => {
-  // const [datal1, setDatal1] = React.useState(fetchDrivers());
-  // const [datal2, setDatal2] = React.useState(fetchTrafficViolations());
-  // const [datal3, setDatal3] = React.useState(fetchAppeals());
-  // const [datal4, setDatal4] = React.useState(fetchEmployees());
-
-  // const [datab2, setDatab2] = React.useState(
-  //   fetchFirebaseTrafficViolationsByDriverId("ADMDCgbQt5CKuMhzEqdk"),
-  // );
-  // const [datab3, setDatab3] = React.useState(
-  //   fetchFirebaseTrafficViolationsByAppealId("1E817SKu37haAgBjb28W"),
-  // );
-
-  // const [data1, setData1] = React.useState(fetchDriverById("ADMDCgbQt5CKuMhzEqdk"));
-  // const [data2, setData2] = React.useState(fetchTrafficViolationById("01EM9WHenHzZC8qYSeQN"));
-  // const [data3, setData3] = React.useState(fetchAppealById("1E817SKu37haAgBjb28W"));
-  // const [data4, setData4] = React.useState(fetchEmployeeById("FHfDoe3izWTHU1MMm3Je"));
-
   return <UnderConstruction />;
 };
 
