8cc1527ca4fea66cab73f23fd4e2576eca752028
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2024-02-27 15:02:26 -0300

    Fix selfsuspensive tag position

--- src/components/ui/DetailsPageWrapper.tsx
+++ src/components/ui/DetailsPageWrapper.tsx
@@ -24,7 +24,7 @@ const DetailsPageWrapper = ({
 
   return (
     <section className="flex h-full max-w-full flex-col gap-6 rounded-lg bg-white p-4 dark:bg-black-700 sm:px-6 sm:py-5">
-      {pageTag ? <div>{pageTag}</div> : null}
+      {pageTag ? <div className="pl-3.5">{pageTag}</div> : null}
       <div className="flex flex-col-reverse items-start justify-between gap-4 sm:flex-row sm:gap-5">
         {pageKpi}
         {pageActions}
