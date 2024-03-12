4a4c798a8baa74184fc97063a64cbbb58b633851
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2024-03-12 19:51:07 -0300

    Add max birthdate input

--- src/utils/getMaxBirthdate.ts
+++ src/utils/getMaxBirthdate.ts
@@ -1,8 +1,8 @@
 export function getMaxBirthdateFor18YearsOld(): string | undefined {
   try {
     const today = new Date();
-    const eighteenYearsAgo = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
-    return eighteenYearsAgo.toISOString().split("T")[0];
+    const maxBirthdate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+    return maxBirthdate.toISOString().split("T")[0];
   } catch (error) {
     console.error(error);
   }
